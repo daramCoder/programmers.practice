077b3923eb01bcceff2a5a91582493cb0bbee54a
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-08-03 22:10:26 +0900

    Initial commit

---
+++ src/num2_multiple.java
@@ -0,0 +1,9 @@
+public class num2_multiple {
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+
+            return num1 * num2;
+        }
+    }
+}
