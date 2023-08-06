e3e62b520d654a54e8d18a55d1a3112f4a08ff22
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-08-06 21:51:27 +0900

    프로그래머스 문제 추가 5 개 풀기 _4일차

--- src/num_23_converse_arrayList.java
+++ src/num_23_converse_arrayList.java
@@ -4,14 +4,16 @@ public class num_23_converse_arrayList {
 
     class Solution {
         public int[] solution(int[] num_list) {
-            int[] answer = {};
-
-            for(int i = 0; i < num_list.length; i++){
-                answer[num_list.length - i] = i;
-
+            int[] answer = new int [num_list.length] ;  // 이렇게 길이를 제한해줘야 한다!!
+            for (int i = 0; i < num_list.length; i++) {
+                answer[i] = num_list[num_list.length - i - 1];
             }
-
             return answer;
         }
     }
+
+
 }
+
+
+
---
+++ src/num_25_dot_location.java
@@ -0,0 +1,24 @@
+public class num_25_dot_location {
+    public static void main(String[] args) {
+    }
+    class Solution {
+        public int solution(int[] dot) {
+            int answer = 0;
+
+            if(dot[0] > 0 && dot[1] >0){
+                answer = 1;
+            }
+            else if(dot[0] < 0 && dot[1] >0){
+                answer = 2;
+            }
+            else if(dot[0] < 0 && dot[1] <0){
+                answer = 3;
+            }
+            else{
+                answer = 4;
+            }
+
+            return answer;
+        }
+    }
+}
