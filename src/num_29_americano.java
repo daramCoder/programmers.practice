31aa57d303fc513ba145198b929ac07caf5d1057
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-08-08 06:14:48 +0900

    프로그래머스 문제 1개 풀기 _6일차

---
+++ src/num_29_americano.java
@@ -0,0 +1,17 @@
+public class num_29_americano {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public int[] solution(int money) {
+            int[] answer = new int[2];
+
+            answer[0]  = (int)(money/5500);
+            answer[1] = (int)(money%5500);
+
+            return answer;
+        }
+    }
+
+}
