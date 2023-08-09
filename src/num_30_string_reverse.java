a323d4718c046d789d28ab1fa44db8fe0ba4a6f7
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-08-09 22:01:37 +0900

    프로그래머스 문제 1개 풀기 _7일차

---
+++ src/num_30_string_reverse.java
@@ -0,0 +1,15 @@
+public class num_30_string_reverse {
+    public static void main(String[] args) {
+
+    }
+    class Solution {
+        public String solution(String my_string) {
+            String answer = "";
+
+            for(int i = my_string.length() - 1; i >=0; i-- ){
+                answer = answer + my_string.charAt(i);
+            }
+            return answer;
+        }
+    }
+}
