83eb57ef592e5654c331905e46728f2c876812f7
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-08-11 21:19:54 +0900

    프로그래머스 문제 2개 풀기 _9일차

---
+++ src/num_34_remove_string.java
@@ -0,0 +1,13 @@
+public class num_34_remove_string {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public String solution(String my_string, String letter) {
+            String answer = "";
+            return  my_string.replace(letter, "");
+        }
+    }
+
+}
