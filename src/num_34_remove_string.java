8fc986d5d52b844b007353f1b2a27ae4902f91fb
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-08-12 20:32:53 +0900

    프로그래머스 문제 1개 풀기 _10일차

--- src/num_34_remove_string.java
+++ src/num_34_remove_string.java
@@ -7,6 +7,8 @@ public class num_34_remove_string {
         public String solution(String my_string, String letter) {
             String answer = "";
             return  my_string.replace(letter, "");
+
+            // "letter" 로 했는데 , 프로그래머스에서는 오류가 났다.
         }
     }
 
