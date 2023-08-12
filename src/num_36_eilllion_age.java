eb7e9f47daa5d0cb02cae8cc9ae995a659001da1
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-08-12 22:28:50 +0900

    프로그래머스 문제 2개 풀기 _10일차

---
+++ src/num_36_eilllion_age.java
@@ -0,0 +1,20 @@
+public class num_36_eilllion_age {
+    public static void main(String[] args) {
+
+    }
+
+    class Solution {
+        public String solution(int age) {
+            String answer = "";
+            String age962 = "abcdefghij";
+            String[] ageArr = String.valueOf(age).split("");
+
+            for (int i = 0; i < ageArr.length; i++) {
+                answer += age962.charAt(Integer.valueOf(ageArr[i]));
+            }
+            return answer;
+        }
+    }
+}
+
+
