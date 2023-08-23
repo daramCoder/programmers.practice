cf0cf3bc2c0f11fa017bfaecee2da870a1d196a6
YOOSUNAH <tjsdkyd4027@gmail.com>
2023-08-23 23:21:21 +0900

    프로그래머스 문제 풀기 _DAY16 가장 큰 수 찾기,문자열 계산하기, DAY17 숫자 찾기, n의 배수 고르기, 자릿수 더하기

---
+++ src/num_61_70/num_66_findBigestNum.java
@@ -0,0 +1,23 @@
+package num_61_70;
+
+public class num_66_findBigestNum {
+    public static void main(String[] args) {
+    }
+
+    class Solution {
+        public int[] solution(int[] array) {
+            int[] answer = new int[2];
+            int max = 0;
+
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] >max){
+                    max = array[i];
+                    answer[0] = max;
+                    answer[1] = i;
+                }
+            }
+            return answer;
+        }
+    }
+
+}
---
+++ src/num_61_70/num_70_FindDigit.java
@@ -0,0 +1,22 @@
+package num_61_70;
+
+public class num_70_FindDigit {
+    public static void main(String[] args) {
+
+    }
+
+        public int solution(int n) {
+            int answer = 0;
+
+            String[] arr = String.valueOf(n).split("");
+
+            for (int i =0; i < arr.length; i++){
+                answer += Integer.parseInt(arr[i]);
+            }
+
+            return answer;
+        }
+
+
+
+}
