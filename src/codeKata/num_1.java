4a96615882b108458c4cf0daa8cf917178d2457c
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-04 09:33:57 +0900

    [알고리즘 코드카타] 프로그래머스 문제 풀기 : 두수의 차

---
+++ src/codeKata/num_1.java
@@ -0,0 +1,11 @@
+package codeKata;
+
+public class num_1 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = 0;
+            answer = num1 - num2;
+            return answer;
+        }
+    }
+}
