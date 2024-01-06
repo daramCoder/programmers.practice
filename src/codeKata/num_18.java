34d5d5587a24d968213a3be7ec117530e5ca5e77
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-07 00:21:49 +0900

    [알고리즘 코드카타] 프로그래머스 문제 풀기
    문자열을 정수로 바꾸기

---
+++ src/codeKata/num_18.java
@@ -0,0 +1,12 @@
+package codeKata;
+
+public class num_18 {
+    class Solution {
+        public int solution(String s) {
+            int answer = 0;
+            answer = Integer.parseInt(s);
+            return answer;
+        }
+    }
+
+}
