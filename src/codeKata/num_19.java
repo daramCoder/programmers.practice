f34efde6d937a3073756bbff266c46c2d866ab1e
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-08 10:08:18 +0900

    [알고리즘 코드카타] 프로그래머스 문제 풀기
    문자열을 정수로 바꾸기, 정수 제곱근 판별

---
+++ src/codeKata/num_19.java
@@ -0,0 +1,19 @@
+package codeKata;
+// 정수 제곱근 판별
+//임의의 양의 정수 n에 대해, n이 어떤 양의 정수 x의 제곱인지 아닌지 판단하려 합니다.
+//n이 양의 정수 x의 제곱이라면 x+1의 제곱을 리턴하고, n이 양의 정수 x의 제곱이 아니라면 -1을 리턴하는 함수를 완성하세요
+public class num_19 {
+    class Solution {
+        public long solution(long n) {
+            long answer = 0;
+            double x = Math.sqrt(n); //
+            // Math.sqrt(n) n의 제곱근
+            // Math.pow(n) n의 제곱을 구하는 식
+            if (x % 1 == 0){
+               answer = (long)Math.pow(x+1, 2);
+            }else answer = -1;
+            return answer;
+        }
+    }
+
+}
---
+++ src/codeKata/num_20.java
@@ -0,0 +1,38 @@
+package codeKata;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+// 정수 내림차순으로 배치하기
+// 함수 solution은 정수 n을 매개변수로 입력받습니다.
+// n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.
+public class num_20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        methodA(n);
+    }
+
+    public static long methodA(long n){
+            long answer = 0;
+            String srtN = String.valueOf(n);
+            String[] array = srtN.split("");
+            Arrays.sort(array, Collections.reverseOrder()); // 배열 내림차순으로 정렬
+            // 다시 합친 다음에 정수로
+            StringBuilder str = new StringBuilder();
+            for (String s : array) {
+                str.append(s);
+            }
+             answer = Long.parseLong(String.valueOf(str));
+//           System.out.println(answer);
+            return answer;
+    }
+}
+
+
+
+
+
+
