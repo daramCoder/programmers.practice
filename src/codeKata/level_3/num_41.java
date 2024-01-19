148fc9c0d2f889a3984ba5a8e1b064a43200f5ca
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-01-19 09:30:30 +0900

    [알고리즘 코드카타] 프로그래머스 문제 풀기 : 이상한 문자 만들기

---
+++ src/codeKata/level_3/num_41.java
@@ -0,0 +1,18 @@
+package codeKata.level_3;
+// 이상한 문자 만들기
+public class num_41 {
+    class Solution {
+        public String solution(String s) {
+            String answer = "";
+            String[] arr = s.split("");
+            // s.toUpperCase() 대문자로
+            // s.toLowerCase() 소문자로
+            int index = 0;
+            for (String str : arr) {
+                index = (str.contains(" ") ? 0 : index + 1);  // 1, 2, 3, 0, 1, 2, (예시)
+                answer += index % 2 == 0 ? str.toLowerCase() : str.toUpperCase();
+            }
+            return answer;
+        }
+    }
+}
