8ec0b2f954645add9b1e6f327d0c8de58c1191dd
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-03-14 10:21:55 +0900

    [알고리즘 코드카타] 문제 : 크레인 인형 뽑기

--- src/codeKata/level_4/크레인인형뽑기.java
+++ src/codeKata/level_4/크레인인형뽑기.java
@@ -5,10 +5,11 @@ import java.util.Stack;
 public class 크레인인형뽑기 {
 
     public static void main(String[] args) {
-        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
-        int[] moves = {1,5,3,5,1,2,1,4};
+        int[][] board = {{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2},
+            {3, 5, 1, 3, 1}};
+        int[] moves = {1, 5, 3, 5, 1, 2, 1, 4};
 
-        solution(board, moves);
+        solution2(board, moves);
     }
 
     public static int solution(int[][] board, int[] moves) {
@@ -34,4 +35,34 @@ public class 크레인인형뽑기 {
         System.out.println(answer);
         return answer;
     }
-}
+
+    // 다시 풀기
+    public static int solution2(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+
+        for (int move : moves) {
+            for (int i = 0; i < board.length; i++) {
+                // [][move -1] 에 빈칸이 아닐 경우
+                if (board[i][move - 1] != 0) {
+                    // stack에 있는 것과 동일하면
+                    if (stack.peek() == board[i][move - 1]) {
+                        // stack에서 없애주기
+                        stack.pop();
+                        // 사라진 인형 2개 추가
+                        answer += 2;
+                    } else {
+                        // stack 에 있는 것과 동일하지 않다면, stack에 담아주기
+                        stack.push(board[i][move - 1]);
+                    }
+                    // 꺼낸 인형 board에서 없애주기
+                    board[i][move - 1] = 0;
+                    break; // for문 탈출
+                }
+            }
+        }
+            System.out.println(answer);
+            return answer;
+        }
+    }
