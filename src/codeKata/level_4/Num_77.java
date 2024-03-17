00c1868823f344e54fde98ec2dfa3acab661142f
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-03-17 22:37:58 +0900

    [알고리즘 코드 카타] : 문피보나치 수 문제

---
+++ src/codeKata/level_4/Num_77.java
@@ -0,0 +1,22 @@
+package codeKata.level_4;
+
+public class Num_77 {
+
+    public static void main(String[] args) {
+        int n = 3;
+        fibonachi(n);
+    }
+
+        public static  int fibonachi(int n) {
+            int[] answer = new int[n + 1];
+            answer[0] = 0;
+            answer[1] = 1;
+
+            for (int i = 2; i <= n; i++) {
+                answer[i] = (answer[i - 1] + answer[i - 2]) % 1234567;
+            }
+            System.out.println(answer[n] % 1234567);
+            return answer[n] % 1234567;
+        }
+    }
+
